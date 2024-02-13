f1d30504bbfe18e5af6392f293e55af196eb3f89
atakan arslan <atak.arslan@hotmail.com>
2024-02-13 11:46:13 +0300

    Added Form and Crud Operations

--- src/main/java/com/mvc/web/controller/ClubController.java
+++ src/main/java/com/mvc/web/controller/ClubController.java
@@ -6,6 +6,9 @@ import com.mvc.web.service.ClubService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -28,5 +31,23 @@ public class ClubController {
         Club club = new Club();
         model.addAttribute("club", club);
         return "clubs-create";
+    }
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club){
+        clubService.saveClub(club);
+        return "redirect:/clubs";
+
+    }
+    @GetMapping("/clubs/{clubId}/edit")
+    public String editClubForm(@PathVariable("clubId") long clubId, Model model){
+        ClubDto club = clubService.findClubById(clubId);
+        model.addAttribute("club", club);
+        return "clubs-edit";
+    }
+    @PostMapping("/clubs/{clubId}/edit")
+    public String updateClub(@PathVariable("clubId") Long clubId, @ModelAttribute("club") ClubDto club ){
+        club.setId(clubId);
+        clubService.updateClub(club);
+        return null;
     }
 }
--- src/main/java/com/mvc/web/service/ClubService.java
+++ src/main/java/com/mvc/web/service/ClubService.java
@@ -1,10 +1,15 @@
 package com.mvc.web.service;
 
 import com.mvc.web.dto.ClubDto;
+import com.mvc.web.models.Club;
 
 
 import java.util.List;
 
 public interface ClubService {
     public List<ClubDto> getAllClubs();
+    Club saveClub(Club club);
+    ClubDto findClubById(long clubId);
+
+    void updateClub(ClubDto club);
 }
